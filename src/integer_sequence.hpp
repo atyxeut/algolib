2efcce83e8dca50a3de844cd7ced82e4f4ff1011
atyxeut <atyxeut@outlook.com>
2025-09-10 03:49:59 +0800

    add integer_sequence.hpp and its documentation

---
+++ src/integer_sequence.hpp
@@ -0,0 +1,57 @@
+#ifndef AAL_SRC_INTEGER_SEQUENCE_HPP
+#define AAL_SRC_INTEGER_SEQUENCE_HPP
+
+/* https://github.com/atyxeut/algolib/blob/main/src/integer_sequence.hpp */
+
+#include "aliases.hpp"
+
+namespace aal {
+
+template <typename TInt, TInt... Ints>
+struct integer_sequence
+{
+  using value_type = TInt;
+
+  static constexpr size_t size() noexcept
+  {
+    return sizeof...(Ints);
+  };
+};
+
+template <size_t... Ints>
+using index_sequence = ::aal::integer_sequence<size_t, Ints...>;
+
+namespace details {
+
+template <typename TInt, TInt Begin, TInt End, TInt... Ints>
+struct make_integer_sequence_impl
+{
+  using type = typename ::aal::details::make_integer_sequence_impl<TInt, Begin + 1, End, Ints..., Begin>::type;
+};
+
+template <typename TInt, TInt End, TInt... Ints>
+struct make_integer_sequence_impl<TInt, End, End, Ints...>
+{
+  using type = ::aal::integer_sequence<TInt, Ints...>;
+};
+
+} // namespace details
+
+template <typename TInt, TInt N>
+using make_integer_sequence = typename ::aal::details::make_integer_sequence_impl<TInt, 0, N>::type;
+
+template <typename TInt, TInt N>
+using make_integer_sequence_from_1 = typename ::aal::details::make_integer_sequence_impl<TInt, 1, N + 1>::type;
+
+template <size_t N>
+using make_index_sequence = ::aal::make_integer_sequence<size_t, N>;
+
+template <size_t N>
+using make_index_sequence_from_1 = ::aal::make_integer_sequence_from_1<size_t, N>;
+
+template <typename... Ts>
+using index_sequence_for = ::aal::make_index_sequence<sizeof...(Ts)>;
+
+} // namespace aal
+
+#endif // AAL_SRC_INTEGER_SEQUENCE_HPP
