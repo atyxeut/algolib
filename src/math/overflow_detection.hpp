fece31a601b0f1cc2e0ccce261bbf306e23a13d4
atyxeut <atyxeut@outlook.com>
2025-09-12 03:44:32 +0800

    remove redundant qualified name lookup and use unqualified name lookup

--- src/math/overflow_detection.hpp
+++ src/math/overflow_detection.hpp
@@ -12,17 +12,17 @@ namespace aal {
 template <typename TResult, typename T1, typename T2>
 AAL_CONSTEXPR14 bool iadd_overflows(T1 lhs, T2 rhs) noexcept
 {
-  static_assert(::aal::conjunction<::aal::is_integral<T1>, ::aal::is_integral<T2>>::value, "given operands must be integers");
+  static_assert(conjunction<is_integral<T1>, is_integral<T2>>::value, "given operands must be integers");
   assert(lhs >= 0 && rhs >= 0 && "given operands must be nonnegative");
-  return rhs > ::aal::lim<TResult>::max() || lhs > ::aal::lim<TResult>::max() - rhs;
+  return rhs > lim<TResult>::max() || lhs > lim<TResult>::max() - rhs;
 }
 
 template <typename TResult, typename T1, typename T2>
 AAL_CONSTEXPR14 bool imul_overflows(T1 lhs, T2 rhs) noexcept
 {
-  static_assert(::aal::conjunction<::aal::is_integral<T1>, ::aal::is_integral<T2>>::value, "given operands must be integers");
+  static_assert(conjunction<is_integral<T1>, is_integral<T2>>::value, "given operands must be integers");
   assert(lhs >= 0 && rhs >= 0 && "given operands must be nonnegative");
-  return rhs == 0 ? false : lhs > ::aal::lim<TResult>::max() / rhs;
+  return rhs == 0 ? false : lhs > lim<TResult>::max() / rhs;
 }
 
 } // namespace aal
