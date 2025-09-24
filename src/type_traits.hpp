4bb37d22fac349dd2fea8b4d6f8e049096a80fed
atyxeut <atyxeut@outlook.com>
2025-09-25 00:32:10 +0800

    add aal::is_range

--- src/type_traits.hpp
+++ src/type_traits.hpp
@@ -504,6 +504,46 @@ using unwrap_integral = details::unwrap_integral_impl<T>;
 template <typename T>
 using unwrap_integral_t = typename unwrap_integral<T>::type;
 
+template <typename T, typename = void>
+struct can_call_std_begin : ::std::false_type
+{
+};
+
+template <typename T>
+struct can_call_std_begin<T, void_t<decltype(::std::begin(::std::declval<T&>()))>> : ::std::true_type
+{
+};
+
+#if CPP14
+template <typename T>
+constexpr bool can_call_std_begin_v = can_call_std_begin<T>::value;
+#endif // C++14
+
+template <typename T, typename = void>
+struct can_call_std_end : ::std::false_type
+{
+};
+
+template <typename T>
+struct can_call_std_end<T, void_t<decltype(::std::end(::std::declval<T&>()))>> : ::std::true_type
+{
+};
+
+#if CPP14
+template <typename T>
+constexpr bool can_call_std_end_v = can_call_std_end<T>::value;
+#endif // C++14
+
+template <typename T>
+struct is_range : conjunction<can_call_std_begin<T>, can_call_std_end<T>>
+{
+};
+
+#if CPP14
+template <typename T>
+constexpr bool can_call_std_end_v = can_call_std_end<T>::value;
+#endif // C++14
+
 } // namespace aal
 
 #endif // AAL_SRC_TYPE_TRAITS_HPP
