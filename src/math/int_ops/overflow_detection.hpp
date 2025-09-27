055ca5ace7680c16b626c3844cf7be7fa0569f6e
atyxeut <atyxeut@outlook.com>
2025-09-27 23:59:45 +0800

    remove aliases/numeric_limits.hpp

--- src/math/int_ops/overflow_detection.hpp
+++ src/math/int_ops/overflow_detection.hpp
@@ -3,10 +3,10 @@
 
 /* https://github.com/atyxeut/algolib/blob/main/src/math/overflow_detection.hpp */
 
-#include "../../aliases/numeric_limits.hpp"
 #include "../../macros/constexpr.hpp"
 #include "../../type_traits/integral.hpp"
 #include <cassert>
+#include <limits>
 
 namespace aal {
 
@@ -15,7 +15,7 @@ AAL_CONSTEXPR14 bool iadd_overflows(T1 lhs, T2 rhs) noexcept
 {
   static_assert(conjunction<is_integral<T1>, is_integral<T2>>::value, "given operands must be integers");
   assert(lhs >= 0 && rhs >= 0 && "given operands must be nonnegative");
-  return rhs > lim<TResult>::max() || lhs > lim<TResult>::max() - rhs;
+  return rhs > ::std::numeric_limits<TResult>::max() || lhs > ::std::numeric_limits<TResult>::max() - rhs;
 }
 
 template <typename TResult, typename T1, typename T2>
@@ -23,7 +23,7 @@ AAL_CONSTEXPR14 bool imul_overflows(T1 lhs, T2 rhs) noexcept
 {
   static_assert(conjunction<is_integral<T1>, is_integral<T2>>::value, "given operands must be integers");
   assert(lhs >= 0 && rhs >= 0 && "given operands must be nonnegative");
-  return rhs == 0 ? false : lhs > lim<TResult>::max() / rhs;
+  return rhs == 0 ? false : lhs > ::std::numeric_limits<TResult>::max() / rhs;
 }
 
 } // namespace aal
