b97deebd229343be61b8ad091f4a9a2e5ae54da9
atyxeut <atyxeut@outlook.com>
2025-10-03 17:10:20 +0800

    fix bit.hpp

--- src/math/int_ops/bit.hpp
+++ src/math/int_ops/bit.hpp
@@ -16,36 +16,36 @@ namespace aal {
 
 namespace details {
 
-template <::std::size_t Width, typename T>
-AAL_CONSTEXPR14 auto countl_zero_impl(T x) noexcept -> typename ::std::enable_if<Width == ::std::numeric_limits<u32>::digits, int>::type
+template <size_t Width, typename T>
+AAL_CONSTEXPR14 auto countl_zero_impl(T x) noexcept -> typename std::enable_if<Width == std::numeric_limits<u32>::digits, int>::type
 {
   using unsigned_T = make_unsigned_t<T>;
 #if AAL_MSVC
   unsigned long ret;
   _BitScanReverse(&ret, x);
-  return ::std::numeric_limits<unsigned_T>::digits - ret - 1;
+  return std::numeric_limits<unsigned_T>::digits - ret - 1;
 #else
-  return ::std::numeric_limits<unsigned_T>::digits + __builtin_clz(x) - Width;
+  return std::numeric_limits<unsigned_T>::digits + __builtin_clz(x) - Width;
 #endif // MSVC
 }
 
-template <::std::size_t Width, typename T>
-AAL_CONSTEXPR14 auto countl_zero_impl(T x) noexcept -> typename ::std::enable_if<Width == ::std::numeric_limits<u64>::digits, int>::type
+template <size_t Width, typename T>
+AAL_CONSTEXPR14 auto countl_zero_impl(T x) noexcept -> typename std::enable_if<Width == std::numeric_limits<u64>::digits, int>::type
 {
 #if AAL_MSVC
   unsigned long ret;
   _BitScanReverse64(&ret, x);
-  return ::std::numeric_limits<u64>::digits - ret - 1;
+  return std::numeric_limits<u64>::digits - ret - 1;
 #else
   return __builtin_clzll(x);
 #endif // MSVC
 }
 
-template <::std::size_t Width, typename T>
-AAL_CONSTEXPR14 auto countl_zero_impl(T x) noexcept -> typename ::std::enable_if<Width == ::std::numeric_limits<u128>::digits, int>::type
+template <size_t Width, typename T>
+AAL_CONSTEXPR14 auto countl_zero_impl(T x) noexcept -> typename std::enable_if<Width == std::numeric_limits<u128>::digits, int>::type
 {
 #if AAL_MSVC
-  constexpr int u64_width = ::std::numeric_limits<u128>::digits / 2;
+  constexpr int u64_width = std::numeric_limits<u128>::digits / 2;
   return x >> u64_width == 0 ? u64_width + countl_zero_impl<u64_width>(static_cast<u64>(x)) : countl_zero_impl<u64_width>(static_cast<u64>(x >> u64_width));
 #else
   return __builtin_clzg(static_cast<u128>(x));
@@ -55,24 +55,23 @@ AAL_CONSTEXPR14 auto countl_zero_impl(T x) noexcept -> typename ::std::enable_if
 } // namespace details
 
 template <typename T>
-AAL_CONSTEXPR14 auto countl_zero(T x) noexcept -> typename ::std::enable_if<is_nonbool_integral<T>::value, int>::type
+AAL_CONSTEXPR14 auto countl_zero(T x) noexcept -> typename std::enable_if<is_nonbool_integral<T>::value, int>::type
 {
   assert(x >= 0 && "the given operand must be nonnegative");
-
   using unsigned_T = make_unsigned_t<T>;
   using unsigned_promoted_T = make_unsigned_t<decltype(+x)>;
-  return x == 0 ? ::std::numeric_limits<unsigned_T>::digits : details::countl_zero_impl<::std::numeric_limits<unsigned_promoted_T>::digits>(x);
+  return x == 0 ? std::numeric_limits<unsigned_T>::digits : details::countl_zero_impl<std::numeric_limits<unsigned_promoted_T>::digits>(x);
 }
 
 template <typename T>
-AAL_CONSTEXPR14 auto bit_width(T x) noexcept -> typename ::std::enable_if<is_nonbool_integral<T>::value, int>::type
+AAL_CONSTEXPR14 auto bit_width(T x) noexcept -> typename std::enable_if<is_nonbool_integral<T>::value, int>::type
 {
   using unsigned_T = make_unsigned_t<T>;
-  return ::std::numeric_limits<unsigned_T>::digits - countl_zero(x);
+  return std::numeric_limits<unsigned_T>::digits - countl_zero(x);
 }
 
 template <typename T>
-AAL_CONSTEXPR14 auto ilog2(T x) noexcept -> typename ::std::enable_if<is_nonbool_integral<T>::value, int>::type
+AAL_CONSTEXPR14 auto ilog2(T x) noexcept -> typename std::enable_if<is_nonbool_integral<T>::value, int>::type
 {
   return bit_width(x) - 1;
 }
