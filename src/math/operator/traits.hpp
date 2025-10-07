eca3c747518d395ff298af8142e57610a2fe2075
atyxeut <atyxeut@outlook.com>
2025-10-07 15:32:07 +0800

    add some operator traits

---
+++ src/math/operator/traits.hpp
@@ -0,0 +1,21 @@
+#ifndef AAL_SRC_MATH_OPERATOR_TRAITS_HPP
+#define AAL_SRC_MATH_OPERATOR_TRAITS_HPP
+
+/* https://github.com/atyxeut/algolib/blob/main/src/math/operator/traits.hpp */
+
+#include "categories.hpp"
+
+namespace aal { namespace op {
+
+template <typename TOp>
+struct operator_traits_base
+{
+// clang-format off
+  using category     = typename TOp::category;
+  using operand_type = typename TOp::operand_type;
+// clang-format on
+};
+
+}} // namespace aal::op
+
+#endif // AAL_SRC_MATH_OPERATOR_TRAITS_HPP
