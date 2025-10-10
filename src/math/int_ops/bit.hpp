f16ce2954f8d0ba818b7c5673bf413c83b430303
atyxeut <atyxeut@outlook.com>
2025-10-11 03:28:57 +0800

    minor fix

--- src/math/int_ops/bit.hpp
+++ src/math/int_ops/bit.hpp
@@ -1,6 +1,8 @@
 #ifndef AAL_SRC_MATH_INT_OPS_BIT_HPP
 #define AAL_SRC_MATH_INT_OPS_BIT_HPP
 
+/* https://github.com/atyxeut/algolib/blob/main/src/math/int_ops/bit.hpp */
+
 #include "../../macros/compiler_test.hpp"
 #include "../../macros/constexpr.hpp"
 #include "../../type_traits/integral.hpp"
