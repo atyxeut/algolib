feb61b78d4d0c9dafac4445fd94ae309f687d497
atyxeut <atyxeut@outlook.com>
2025-10-15 02:14:35 +0800

    format

--- fmt_cpp_files.py
+++ fmt_cpp_files.py
@@ -3,7 +3,6 @@
 from pathlib import Path
 import subprocess
 
-
 def fmt(path: Path, dep: int = 0) -> None:
     if path.is_dir():
         if dep == 0 or dep == 1 and path.name == "src" or dep > 1:
@@ -12,7 +11,6 @@ def fmt(path: Path, dep: int = 0) -> None:
     elif path.suffix == ".cpp" or path.suffix == ".hpp":
         print(f"Formatting {path}")
         subprocess.run(["clang-format", "-style=file", "-i", path])
-
 
 fmt(Path(".").resolve())
 print("Done.")
