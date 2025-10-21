036abcb04cec8e9685114c7b77e249995c73b52b
atyxeut <atyxeut@outlook.com>
2025-10-21 22:07:05 +0800

    merge src/macros/compiler_test.hpp and architecture_test.hpp into target.hpp

---
+++ src/macros/target.hpp
@@ -0,0 +1,18 @@
+#ifndef AAL_SRC_MACROS_TARGET_HPP
+#define AAL_SRC_MACROS_TARGET_HPP
+
+/* https://github.com/atyxeut/algolib/blob/main/src/macros/target.hpp */
+
+#if defined(__i386__) || defined(_M_IX86)
+#define AAL_ARCH_X86 1
+#endif // test if the target architecture is x86
+
+#if defined(__x86_64__) || defined(_M_X64)
+#define AAL_ARCH_X86_64 1
+#endif // test if the target architecture is x86_64
+
+#ifdef _MSC_VER
+#define AAL_MSVC 1
+#endif // test if the target compiler is MSVC
+
+#endif // AAL_SRC_MACROS_TARGET_HPP
