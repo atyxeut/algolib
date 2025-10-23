7d32363d53df14bb00e56064a1a57547de0511ea
atyxeut <atyxeut@outlook.com>
2025-10-24 01:16:39 +0800

    minor change

--- fmt_cpp_files.py
+++ fmt_cpp_files.py
@@ -1,4 +1,5 @@
-# format every .cpp/.hpp file using clang-format (requires clang-format 21+), using the formatting rules defined in .clang-format
+# > python3 fmt_cpp_files.py
+#   format every .cpp/.hpp file using clang-format (requires clang-format 21+), using the formatting rules defined in .clang-format
 
 from pathlib import Path
 import subprocess
