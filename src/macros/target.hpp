3f1fc90693aec720197b2b8eafec4af7ce8937d3
atyxeut <atyxeut@outlook.com>
2025-11-13 05:54:16 +0800

    remove redundant macros

--- src/macros/target.hpp
+++ src/macros/target.hpp
@@ -1,7 +1,7 @@
 #ifndef AAL_SRC_MACROS_TARGET_HPP
 #define AAL_SRC_MACROS_TARGET_HPP
 
-/* https://github.com/atyxeut/algolib/blob/main/src/macros/target.hpp */
+/* https://github.com/atyxeut/algolib/blob/cpp20/src/macros/target.hpp */
 
 #if defined(__i386__) || defined(_M_IX86)
 #define AAL_ARCH_X86 1
@@ -15,7 +15,7 @@
 #define AAL_ARCH_X86_64 0
 #endif // test if the target architecture is x86_64
 
-#if defined(_MSC_VER) && __cplusplus >= 201402L
+#if defined(_MSC_VER)
 #define AAL_COMPILER_MSVC 1
 #else
 #define AAL_COMPILER_MSVC 0
