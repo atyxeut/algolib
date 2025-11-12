37d0935ff94e927f637d9931ef975b1dbb504d1b
atyxeut <atyxeut@outlook.com>
2025-11-12 17:41:12 +0800

    improve macro defs

--- src/macros/cpp_version.hpp
+++ src/macros/cpp_version.hpp
@@ -7,18 +7,26 @@
 
 #if __cplusplus >= 201402L
 #define AAL_CPP14 1
+#else
+#define AAL_CPP14 0
 #endif // test if the current C++ version is C++14 or above
 
 #if __cplusplus >= 201703L
 #define AAL_CPP17 1
+#else
+#define AAL_CPP17 0
 #endif // test if the current C++ version is C++17 or above
 
 #if __cplusplus >= 202002L
 #define AAL_CPP20 1
+#else
+#define AAL_CPP20 0
 #endif // test if the current C++ version is C++20 or above
 
 #if __cplusplus >= 202302L
 #define AAL_CPP23 1
+#else
+#define AAL_CPP23 0
 #endif // test if the current C++ version is C++23 or above
 
 #endif // AAL_SRC_MACROS_CPP_VERSION_HPP
--- src/macros/target.hpp
+++ src/macros/target.hpp
@@ -5,14 +5,20 @@
 
 #if defined(__i386__) || defined(_M_IX86)
 #define AAL_ARCH_X86 1
+#else
+#define AAL_ARCH_X86 0
 #endif // test if the target architecture is x86
 
 #if defined(__x86_64__) || defined(_M_X64)
 #define AAL_ARCH_X86_64 1
+#else
+#define AAL_ARCH_X86_64 0
 #endif // test if the target architecture is x86_64
 
 #if defined(_MSC_VER) && __cplusplus >= 201402L
 #define AAL_COMPILER_MSVC 1
+#else
+#define AAL_COMPILER_MSVC 0
 #endif // test if the target compiler is MSVC (MSVC STL requires at least C++14)
 
 #endif // AAL_SRC_MACROS_TARGET_HPP
