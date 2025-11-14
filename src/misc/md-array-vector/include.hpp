15139c2a1b24e56017a1335283b090076e9546f8
atyxeut <atyxeut@outlook.com>
2025-11-14 21:19:03 +0800

    some improvement

--- src/misc/md-array-vector/include.hpp
+++ src/misc/md-array-vector/include.hpp
@@ -6,7 +6,6 @@
 #include "../../concepts/integral.hpp"
 #include "../../type-traits/is_std_array.hpp"
 #include "detail.hpp"
-#include <concepts>
 #include <utility>
 
 // provide type alias templates and helper function templates to ease the creation of multidimentional std::array and std::vector
@@ -14,65 +13,62 @@
 namespace aal {
 
 // aal::array<int, 3, 5, 2> arr3d {};
-// same as: std::array<std::array<std::array<int, 2>, 5>, 3> arr3d {};
+//   same as: std::array<std::array<std::array<int, 2>, 5>, 3> arr3d {};
 template <typename T, std::size_t... Dims>
 using array = detail::array_impl<T, Dims...>::type;
 
 // aal::vector<int> vec1d;
-// same as: std::vector<int> vec1d;
+//   same as: std::vector<int> vec1d;
 // aal::vector<int, 4> vec4d;
-// same as: std::vector<std::vector<std::vector<std::vector<int>>>> vec4d;
+//   same as: std::vector<std::vector<std::vector<std::vector<int>>>> vec4d;
 template <typename T, std::size_t DimCnt = 1>
 using vector = detail::vector_impl<T, DimCnt>::type;
 
-// base case of aal::fill_array
-// std::array<int, 10> arr;
-// aal::fill_array(arr, -1); calls this overload
-template <typename TElem, std::size_t Dim, typename T> requires std::convertible_to<T, TElem>
-constexpr void fill_array(std::array<TElem, Dim>& arr, const T& val)
-{
-  arr.fill(static_cast<TElem>(val));
-}
-
-// set every element of a aal::array to val
 // aal::array<int, 3, 5, 2, 10> arr4d;
 // int val = -1;
 // aal::fill_array(arr4d, val);
-template <typename TArr, std::size_t Dim, typename T> requires is_std_array_v<TArr>
-constexpr void fill_array(std::array<TArr, Dim>& arr, const T& val)
+//   set every element of a aal::array to val
+template <typename TElem, std::size_t Dim, typename T>
+constexpr void fill_array(std::array<TElem, Dim>& arr, const T& val)
 {
-  for (auto& inner_arr : arr)
-    fill_array(inner_arr, val);
+  if constexpr (is_std_array_v<TElem>) {
+    for (auto& inner_arr : arr)
+      fill_array(inner_arr, val);
+  }
+  else {
+    arr.fill(static_cast<TElem>(val));
+  }
 }
 
 // auto arr4d = aal::make_array<int, 5, 8, 3, 2>(val);
-// combines aal::array<int, 5, 8, 3, 2> arr4d;
-//      and aal::fill_array(arr4d, val);
+//   combines aal::array<int, 5, 8, 3, 2> arr4d;
+//        and aal::fill_array(arr4d, val);
 template <typename TElem, std::size_t... Dims, typename T>
-constexpr auto make_array(const T& val)
+[[nodiscard]] constexpr auto make_array(const T& val)
 {
   array<TElem, Dims...> arr;
   fill_array(arr, static_cast<TElem>(val));
   return arr;
 }
 
-// base case of aal::make_vector
-// auto vec = aal::make_vector<int>(x, -1); calls this overload
-template <typename TElem, standard_integral TDim, typename T>
-auto make_vector(TDim size, const T& val)
-{
-  return std::vector<TElem>(static_cast<std::size_t>(size), static_cast<TElem>(val));
-}
-
 // auto vec3d = aal::make_vector<int>(x, y, z, 1);
-// same as: auto vec3d = std::vector<std::vector<std::vector<int>>>(
-//                         x,
-//                         std::vector<std::vector<int>>(y, std::vector<int>(z, 1))
-//                       );
-template <typename TElem, standard_integral TDim, typename... Ts> requires (sizeof...(Ts) > 1)
-auto make_vector(TDim first_dim_size, Ts&&... args)
+//   same as: auto vec3d = std::vector<std::vector<std::vector<int>>>(
+//                           x,
+//                           std::vector<std::vector<int>>(y, std::vector<int>(z, 1))
+//                         );
+// constexpr usage example (error if aal::make_vector is not constexpr):
+//   std::cout << [](const std::vector<int>& vec, int sum = 0) consteval
+//   {
+//     std::ranges::for_each(vec, [&sum](int elem) { return sum += elem; });
+//     return sum;
+//   }(aal::make_vector<int>(10, -1)) << "\n";
+template <typename TElem, standard_integral TDim, typename... Ts> requires (sizeof...(Ts) > 0)
+[[nodiscard]] constexpr auto make_vector(TDim first_dim_size, Ts&&... args)
 {
-  return std::vector<vector<TElem, sizeof...(Ts) - 1>>(static_cast<std::size_t>(first_dim_size), make_vector<TElem>(std::forward<Ts>(args)...));
+  if constexpr (sizeof...(Ts) == 1)
+    return std::vector(static_cast<std::size_t>(first_dim_size), static_cast<TElem>(args)...);
+  else
+    return std::vector(static_cast<std::size_t>(first_dim_size), make_vector<TElem>(std::forward<Ts>(args)...));
 }
 
 } // namespace aal
