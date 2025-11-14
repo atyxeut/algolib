bafa5c08be2f0a65925ccc38f36fb159a6911d4e
atyxeut <atyxeut@outlook.com>
2025-11-14 22:22:25 +0800

    simplify macros

--- src/aliases/floating_point.hpp
+++ src/aliases/floating_point.hpp
@@ -1,9 +1,7 @@
 #ifndef AAL_SRC_ALIASES_FLOATING_POINT_HPP
 #define AAL_SRC_ALIASES_FLOATING_POINT_HPP
 
-/* https://github.com/atyxeut/algolib/blob/cpp20/src/aliases/floating_point.hpp */
-
-#include "../macros/target.hpp"
+/* https://github.com/atyxeut/algolib/blob/main/src/aliases/floating_point.hpp */
 
 // precision (significant digits)
 // f32: 6 to 9, f64: 15 to 17, f80: 18 to 20, f128: 33 to 35
@@ -15,7 +13,7 @@ using f80 = long double;
 // f128 is provided by compilers, and is not supported by standard type traits
 // there is no f128 for MSVC
 // for convenience, we consider that f128 only works on x86 or x86_64
-#if AAL_COMPILER_MSVC || !AAL_ARCH_X86 && !AAL_ARCH_X86_64
+#if defined(_MSC_VER) || (!defined(__i386__) && !defined(__x86_64__))
 namespace aal { namespace detail {
 
 struct fake_f128;
@@ -24,7 +22,7 @@ struct fake_f128;
 
 using f128 = ::aal::detail::fake_f128;
 #else
-// details (GCC and Clang): https://gcc.gnu.org/onlinedocs/gcc/Floating-Types.html
+// see https://gcc.gnu.org/onlinedocs/gcc/Floating-Types.html for details (GCC and Clang)
 __extension__ using f128 = __float128;
 #endif // MSVC compiler or not x86(_64) architecture
 
