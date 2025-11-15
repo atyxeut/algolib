8f18523b6674415e10f3bcf871b549f46ee5d81b
atyxeut <atyxeut@outlook.com>
2025-11-16 01:23:32 +0800

    remove MSVC support

--- src/aliases/floating_point.hpp
+++ src/aliases/floating_point.hpp
@@ -11,9 +11,9 @@ using f64 = double;
 using f80 = long double;
 
 // f128 is provided by compilers, and is not supported by standard type traits
-// there is no f128 for MSVC
+// see https://gcc.gnu.org/onlinedocs/gcc/Floating-Types.html for details
 // for convenience, we consider that f128 only works on x86 or x86_64
-#if defined(_MSC_VER) || (!defined(__i386__) && !defined(__x86_64__))
+#if !defined(__i386__) && !defined(__x86_64__)
 namespace aal { namespace detail {
 
 struct fake_f128;
@@ -22,8 +22,7 @@ struct fake_f128;
 
 using f128 = ::aal::detail::fake_f128;
 #else
-// see https://gcc.gnu.org/onlinedocs/gcc/Floating-Types.html for details (GCC and Clang)
 __extension__ using f128 = __float128;
-#endif // MSVC compiler or not x86(_64) architecture
+#endif // not x86(_64) architecture
 
 #endif // AAL_SRC_ALIASES_FLOATING_POINT_HPP
--- src/math/int-ops/bit/detail.hpp
+++ src/math/int-ops/bit/detail.hpp
@@ -4,50 +4,24 @@
 #include "../../../type-traits/integral.hpp"
 #include <limits>
 
-#ifdef _MSC_VER
-#include <intrin.h>
-#endif // MSVC compiler
-
-// docs of compiler built-in bit operation:
-//   https://gcc.gnu.org/onlinedocs/gcc/Bit-Operation-Builtins.html
-//   https://learn.microsoft.com/en-us/cpp/intrinsics/bitscanreverse-bitscanreverse64?view=msvc-170
-
 namespace aal { namespace detail { namespace bit {
 
 template <std::size_t Width, typename T>
-auto countl_zero_impl(T x) noexcept -> typename std::enable_if<Width == std::numeric_limits<u32>::digits, int>::type
+constexpr auto countl_zero_impl(T x) noexcept -> typename std::enable_if<Width == std::numeric_limits<u32>::digits, int>::type
 {
-  constexpr int max_digits = std::numeric_limits<make_unsigned_t<T>>::digits;
-#ifdef _MSC_VER
-  unsigned long ret = 0;
-  _BitScanReverse(&ret, static_cast<unsigned long>(x));
-  return static_cast<int>(max_digits - ret - 1);
-#else
-  return static_cast<int>(max_digits + __builtin_clz(x) - Width);
-#endif // MSVC compiler
+  return std::numeric_limits<make_unsigned_t<T>>::digits + __builtin_clz(x) - static_cast<int>(Width);
 }
 
 template <std::size_t Width, typename T>
 auto countl_zero_impl(T x) noexcept -> typename std::enable_if<Width == std::numeric_limits<u64>::digits, int>::type
 {
-#ifdef _MSC_VER
-  unsigned long ret = 0;
-  _BitScanReverse64(&ret, static_cast<unsigned long long>(x));
-  return static_cast<int>(std::numeric_limits<u64>::digits - ret - 1);
-#else
   return __builtin_clzll(x);
-#endif // MSVC compiler
 }
 
 template <std::size_t Width, typename T>
 auto countl_zero_impl(T x) noexcept -> typename std::enable_if<Width == std::numeric_limits<u128>::digits, int>::type
 {
-#ifdef _MSC_VER
-  constexpr int u64_width = std::numeric_limits<u128>::digits / 2;
-  return x >> u64_width == 0 ? u64_width + countl_zero_impl<u64_width>(static_cast<u64>(x)) : countl_zero_impl<u64_width>(static_cast<u64>(x >> u64_width));
-#else
   return __builtin_clzg(static_cast<u128>(x));
-#endif // MSVC compiler
 }
 
 }}} // namespace aal::detail::bit
