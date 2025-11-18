7dcc5224af3439d6a50db960e8def2a3fd3c0712
atyxeut <atyxeut@outlook.com>
2025-11-19 04:25:20 +0800

    replace SFINAE with concept

--- src/type-trait/operator.hpp
+++ src/type-trait/operator.hpp
@@ -18,13 +18,16 @@ struct binary_operator_tag
 namespace detail {
 
 // follow the STL design, provide an empty class for invalid types, make it friendly to tag dispatch
-template <typename, typename = void>
+template <typename>
 struct operator_traits_impl
 {
 };
 
-template <typename T>
-struct operator_traits_impl<T, std::void_t<typename T::operator_category, typename T::operand_type>>
+template <typename T> requires requires {
+  typename T::operator_category;
+  typename T::operand_type;
+}
+struct operator_traits_impl<T>
 {
   using operator_category = T::operator_category;
   using operand_type = T::operand_type;
