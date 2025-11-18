9614fc4e50e89e3724adcd39162b395b668a5ce9
atyxeut <atyxeut@outlook.com>
2025-11-18 20:28:59 +0800

    minor update

--- src/concept/io.hpp
+++ src/concept/io.hpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #ifndef AAL_SRC_CONCEPT_IO_HPP
 #define AAL_SRC_CONCEPT_IO_HPP
 
@@ -11,3 +12,24 @@ namespace aal {
 } // namespace aal
 
 #endif // AAL_SRC_CONCEPT_IO_HPP
+=======
+#ifndef AAL_SRC_CONCEPT_IO_HPP
+#define AAL_SRC_CONCEPT_IO_HPP
+
+/* https://github.com/atyxeut/algolib/blob/cpp20/src/concept/io.hpp */
+
+#include "../type-trait/io.hpp"
+
+namespace aal {
+
+// check if T has a dedicated overload for operator << to satisfy this concept
+// for what is "dedicated", see this example:
+// for type int, operator <<(..., int) is, while operator <<(..., TArg&&) is not,
+//   even if the latter can actually be selected by overload resolution when the former does not exist
+template <typename TChar, typename T>
+concept ostream_default_interactable = requires(std::basic_ostream<TChar>& ostr, T&& obj) { ostr << obj; };
+
+} // namespace aal
+
+#endif // AAL_SRC_CONCEPT_IO_HPP
+>>>>>>> Stashed changes
--- src/io/range/out.hpp
+++ src/io/range/out.hpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #ifndef AAL_SRC_IO_RANGE_OUT_HPP
 #define AAL_SRC_IO_RANGE_OUT_HPP
 
@@ -54,3 +55,64 @@ decltype(auto) operator <<(std::basic_ostream<TChar>& ostr, TRange&& range)
 }
 
 #endif // AAL_SRC_IO_RANGE_OUT_HPP
+=======
+#ifndef AAL_SRC_IO_RANGE_OUT_HPP
+#define AAL_SRC_IO_RANGE_OUT_HPP
+
+/* https://github.com/atyxeut/algolib/blob/cpp20/src/io/range/out.hpp */
+
+#include "../../concept/io.hpp"
+#include <cstddef>
+#include <iterator>
+#include <ranges>
+#include <string>
+#include <utility>
+
+namespace aal {
+
+template <typename TChar, std::ranges::input_range TRange>
+int print(std::basic_ostream<TChar>& ostr, TRange&& range, std::basic_string_view<TChar> delim, bool never_second_case = true)
+{
+  using value_type = std::iter_value_t<decltype(std::begin(range))>;
+
+  // element of the range can be printed by the specified ostr by default
+  // e.g. std::vector<std::string> can, but std::vector<std::pair<int, int>> can not
+  if constexpr (ostream_default_interactable<TChar, value_type>) {
+    auto cur_delim = never_second_case ? delim : std::basic_string_view<TChar> {std::basic_string<TChar>(1, static_cast<TChar>(' '))};
+    for (auto it = std::begin(range), it_end = std::end(range); it != it_end; ++it)
+      ostr << *it << (std::next(it) == it_end ? std::basic_string_view<TChar> {} : cur_delim);
+    return 1;
+  }
+
+  // element type of the range is non-default-printable range
+  // e.g. std::vector<std::array<int, 4>>
+  if constexpr (std::ranges::input_range<value_type>) {
+    int reverse_dep = 0;
+    for (auto it = std::begin(range), it_end = std::end(range); it != it_end; ++it) {
+      reverse_dep = print(ostr, *it, delim, never_second_case);
+      auto cur_delim = std::basic_string<TChar>(static_cast<std::size_t>(reverse_dep), *std::begin(delim));
+      ostr << (std::next(it) == it_end ? std::basic_string_view<TChar> {} : cur_delim);
+    }
+    return reverse_dep + 1;
+  }
+}
+
+} // namespace aal
+
+// call aal::print with a space or a new line character as the delimiter (depending on whether it is a nested range)
+template <typename TChar, std::ranges::input_range TRange>
+// avoid ambiguous overloads when TRange is std::string, const char[N], ...
+requires (!::aal::ostream_default_interactable<TChar, TRange>)
+decltype(auto) operator <<(std::basic_ostream<TChar>& ostr, TRange&& range)
+{
+  using value_type = std::iter_value_t<decltype(std::begin(range))>;
+
+  ::aal::print(
+    ostr, std::forward<TRange>(range),
+    std::basic_string_view<TChar> {std::basic_string<TChar>(1, static_cast<TChar>(::aal::ostream_default_interactable<TChar, value_type> ? ' ' : '\n'))}
+  );
+  return ostr;
+}
+
+#endif // AAL_SRC_IO_RANGE_OUT_HPP
+>>>>>>> Stashed changes
