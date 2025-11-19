782fb6646a0adcb9cd8d3449805360501b62fc6a
atyxeut <atyxeut@outlook.com>
2025-11-20 03:06:11 +0800

    improve oprerator gcd definition

--- src/math/operator/gcd.hpp
+++ src/math/operator/gcd.hpp
@@ -3,18 +3,21 @@
 
 /* https://github.com/atyxeut/algolib/blob/cpp20/src/math/operator/gcd.hpp */
 
+#include <tuple>
+
 #include "../../type-trait/operator.hpp"
 #include "../integral/basic-operation/overflow_detection.hpp"
 
 namespace aal { namespace op {
 
-namespace detail {
-
-template <typename T>
-struct gcd_impl
+// class template representing operator gcd
+// auto gcd = aal::op::gcd<int> {};
+// int ans = gcd(3, 6);
+template <nonbool_integral T>
+struct gcd
 {
   using operator_category = binary_operator_tag;
-  using operand_type = T;
+  using operand_type = std::tuple<T, T>;
 
   static constexpr T identity_elem = 0; // gcd(0, a) = a
   static constexpr T absorbing_elem = 1; // gcd(1, a) = 1
@@ -30,35 +33,24 @@ struct gcd_impl
   }
 };
 
-template <typename T>
-struct lcm_impl
+// class template representing operator lcm
+// auto lcm = aal::op::lcm<int> {};
+// int ans = lcm(3, 18);
+template <nonbool_integral T>
+struct lcm
 {
   using operator_category = binary_operator_tag;
-  using operand_type = T;
+  using operand_type = std::tuple<T, T>;
 
   static constexpr T identity_elem = 1; // lcm(1, a) = a
   static constexpr T absorbing_elem = 0; // lcm(0, a) = 0
 
   [[nodiscard]] constexpr T operator ()(T a, T b) const noexcept
   {
-    assert(!overflows::imul<T>(a / gcd_impl<T> {}(a, b), b) && "the lcm cannot be represented");
-    return a / gcd_impl<T> {}(a, b) * b;
+    assert(!overflows::imul<T>(a / gcd<T> {}(a, b), b) && "the lcm cannot be represented");
+    return a / gcd<T> {}(a, b) * b;
   }
 };
-
-} // namespace detail
-
-// class template representing operator gcd
-// auto gcd = aal::op::gcd<int> {};
-// int ans = gcd(3, 6);
-template <nonbool_integral T>
-using gcd = detail::gcd_impl<T>;
-
-// class template representing operator lcm
-// auto lcm = aal::op::lcm<int> {};
-// int ans = lcm(3, 18);
-template <nonbool_integral T>
-using lcm = detail::lcm_impl<T>;
 
 }} // namespace aal::op
 
