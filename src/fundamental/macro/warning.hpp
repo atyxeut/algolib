2add83072abc09d60775eab6eef7b112b7e344e4
atyxeut <atyxeut@outlook.com>
2025-12-02 18:17:22 +0800

    adjust macro style

--- src/fundamental/macro/warning.hpp
+++ src/fundamental/macro/warning.hpp
@@ -5,15 +5,15 @@
 
 #if defined(_MSC_VER)
   #define AAL_INT_WCONVERSION_WCOMPARE_PUSH() __pragma(warning(push)) \
-                                            __pragma(warning(disable: 4018)) \
-                                            __pragma(warning(disable: 4267)) \
-                                            __pragma(warning(disable: 4389))
+                                              __pragma(warning(disable: 4018)) \
+                                              __pragma(warning(disable: 4267)) \
+                                              __pragma(warning(disable: 4389))
   #define AAL_INT_WCONVERSION_WCOMPARE_POP() __pragma(warning(pop))
 #else // gcc or clang
   #define AAL_INT_WCONVERSION_WCOMPARE_PUSH() _Pragma("GCC diagnostic push") \
-                                            _Pragma("GCC diagnostic ignored \"-Wsign-conversion\"") \
-                                            _Pragma("GCC diagnostic ignored \"-Wsign-compare\"") \
-                                            _Pragma("GCC diagnostic ignored \"-Wshorten-64-to-32\"")
+                                              _Pragma("GCC diagnostic ignored \"-Wsign-conversion\"") \
+                                              _Pragma("GCC diagnostic ignored \"-Wsign-compare\"") \
+                                              _Pragma("GCC diagnostic ignored \"-Wshorten-64-to-32\"")
   #define AAL_INT_WCONVERSION_WCOMPARE_POP() _Pragma("GCC diagnostic pop")
 #endif // msvc
 
